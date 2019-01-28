05b6c771dab991864cf8f61501cd96f3b99351cf
usmcamgrimm <usmcamgrimm@gmail.com>
2019-01-28 06:50:17 -0500

    WDD221

---
+++ labs/week2/js/plants.js
@@ -0,0 +1,11 @@
+/*
+	Adrian Grimm
+	WDD221 - Web Development with JavaScript
+	Week 2 Lab
+	1/26/2019
+*/
+
+//Variable Definition of img src files
+let blanket = "images/blanket.jpg";
+let bluestem = "images/bluestem.jpg";
+let rugosa = "images/rugosa.jpg";
