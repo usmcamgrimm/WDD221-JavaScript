905ac97713e9b2497ce6583b7653cb7c835fca07
usmcamgrimm <usmcamgrimm@gmail.com>
2019-02-23 10:32:41 -0500

    Week 6

---
+++ labs/week6/js/week6Lab.js
@@ -0,0 +1,17 @@
+//1
+{
+    console.log(i)
+}
+
+//2
+var num = 6;
+if (num % 2 === 0) {
+	console.log(num + 5)
+} else {
+		console.log("That's not an even number.");
+	}
+
+//3
+var i = 0;
+for(let i=0; i < 5; i++)
+console.log(i);
