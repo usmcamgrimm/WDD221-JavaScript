07c2d5e91b6e2d12d1510bbd04855fbb55521752
usmcamgrimm <usmcamgrimm@gmail.com>
2019-03-02 14:11:41 -0500

    Week 7

--- labs/week6/js/week6Lab.js
+++ labs/week6/js/week6Lab.js
@@ -1,4 +1,5 @@
 //1
+for (let i=0; i < 5; i++) //changing the ">" sign to "<" creates a loop of 5
 {
     console.log(i)
 }
@@ -10,8 +11,11 @@ if (num % 2 === 0) {
 } else {
 		console.log("That's not an even number.");
 	}
+//For #2 I re-wrote the function and added the variable for the modulus operation.
+//I added the else statement so that if the variable was changed to ask for user input, it would be there.
 
 //3
 var i = 0;
 for(let i=0; i < 5; i++)
 console.log(i);
+//I added the variable declaration for i and changed the way the function calls the loop. This way it will create the 5-number loop in the console.
