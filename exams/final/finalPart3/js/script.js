915a06c1462eac446bbd5de0992167901e860d6b
usmcamgrimm <usmcamgrimm@gmail.com>
2019-08-05 06:31:02 -0400

    Cleanup

--- exams/final/finalPart3/js/script.js
+++ exams/final/finalPart3/js/script.js
@@ -1,3 +1,10 @@
+/*
+    Adrian Grimm
+    WDD221 JavaScript
+    Final
+    5/8/2019
+*/
+
 let wordLetters     = ['L', 'I', 'O', 'N']; //create an array for the letters in the word
 let guessedLetters  = ['_', '_', '_', '_']; //create an array for the letters' spaces
 
@@ -21,7 +28,7 @@ function guessLetter(letter)
     }
     if (goodGuess) //In the case of a correctly guessed letter:
     {
-        alert('Great Job! You found a letter!'); //Set as an alaert pop up to show a successful guess
+        alert('Great Job! You found a letter: ' + letter); //Set as an alaert pop up to show a successful guess
         document.querySelector("p").innerHTML = guessedLetters.join(''); //Displays correct letters and underscores on the browser window
         if (!moreToGuess) //If there are no more letters to guess: 
         {
@@ -29,7 +36,7 @@ function guessLetter(letter)
         } 
     } else 
     {
-        alert('Oh my, that is not a good choice!'); //alert pops up notifying incorrect guess
+        alert('Oh my, that is not a good choice: ' + letter); //alert pops up notifying incorrect guess
     }
 }
 // The following are letters, simulating a guess
@@ -38,3 +45,5 @@ guessLetter('T');
 guessLetter('I');
 guessLetter('O');
 guessLetter('N');
+
+console.log('The work I submitted on this page is incomplete and I apologize. I have been unable to gind a way to get this working as a word game.  I look forward to finishing this in the future!');
